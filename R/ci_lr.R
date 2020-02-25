e053e30dcbe707a83ef8c8a479035db4941f053a
Jack Quach <jackquach410@gmail.com>
2020-02-25 02:57:43 -0400

    remove stop, add quasibinomial

--- R/ci_lr.R
+++ R/ci_lr.R
@@ -1,20 +1,19 @@
 # function to get OR and IRR
 ci_lr <- function(GLM.RESULT, digits = 4) {
-
+  
   if (GLM.RESULT$family$family == "binomial") {
     LABEL <- "OR"
   } else if (GLM.RESULT$family$family == "poisson") {
     LABEL <- "RR"
-  } else {
-    stop("Not logistic or Poisson model")
-  }
-
+  } else if (GLM.RESULT$family$family == "quasibinomial")
+    LABEL <- "OR"
+  
   COEF      <- stats::coef(GLM.RESULT)
   CONFINT   <- stats::confint.default(GLM.RESULT)
   TABLE     <- cbind(coef=COEF, CONFINT)
   TABLE.EXP <- round(exp(TABLE), digits)
-
+  
   colnames(TABLE.EXP)[1] <- LABEL
-
+  
   TABLE.EXP
 }
